aa43711a8ad9a7c591291101134a437d71b9e6a0
junhyeong.kim <junhyeong.kim@brainpad.co.jp>
2023-04-07 01:03:24 +0900

    add get_num_params method

--- src/model.py
+++ src/model.py
@@ -151,8 +151,17 @@ class GPT(nn.Module):
             if pn.endswith('c_proj.weight'):
                 nn.init.normal_(p, mean=0.0, std=0.02/math.sqrt(2 * config.n_layer))
 
-    def get_num_params(self):
-        pass
+    def get_num_params(self, non_embedding=True):
+        """
+        Return the number of parameters in the model.
+        For non-embedding count (default), the position embeddings get subtracted.
+        The token embeddings would too, except due to the parameter sharing these
+        params are actually used as weights in the final layer, so we include them.
+        """
+        n_params = sum(p.numel() for p in self.parameters())
+        if non_embedding:
+            n_params -= self.transformer.wpe.weight.numel()
+        return n_params
     
     def _init_weights(self, module):
         if isinstance(module, nn.Linear):
